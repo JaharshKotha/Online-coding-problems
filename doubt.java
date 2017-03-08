8ed3f60a3b0e1150595dee177a30b0ec17b1069c
Jaharsh Samayan Kotha Venkata Sadha <jaharsh93@gmail.com>
2017-03-07 17:28:47 -0700

    Create doubt.java

---
+++ doubt.java
@@ -0,0 +1,60 @@
+/*
+ * Complete the function below.
+ */
+public  class Node{
+    
+    int val;
+    Node left;
+    Node right;
+    Node(int x)
+        {
+        val = x;
+    }
+}
+
+public static Node root = null;
+
+    static void createBST(int[] keys) {
+        int len = keys.length;
+       
+         if(root == null)
+       { root = new Node() ;
+        root.val = keys[0];
+       }
+        for(int i=1;i<len;i++)
+            {
+            insert(root,keys[i]);
+            
+        }
+        
+        return root;
+
+    }
+
+public static void insert(Node root, int x)
+    {
+    Node t = new Node(t);
+    Node prev = root;
+    while(root!=null)
+        {
+        prev = root;
+        if(x > root.val)
+            {
+            root = root.right;
+        }
+        else
+            {
+            root = root.left;
+        }
+    }
+    
+    if(prev.val > x)
+        {
+        prev.left = t;
+    }
+    else
+        {
+        prev.right = t;
+    }
+}
+
