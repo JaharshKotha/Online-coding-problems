a837263e346780ba53df315d583171ba0e700c1c
Jaharsh Samayan <jaharsh93@gmail.com>
2017-12-24 16:12:00 -0700

    https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/discuss/

--- rotated_array_with_duplicates.java
+++ rotated_array_with_duplicates.java
@@ -10,45 +10,25 @@ class Solution {
  {
      
      int m = (l+h)/2;
-     System.out.println(m);
+ 
+     //Edge conditions
      if(m==0)
          return (nums[0]>nums[1])?nums[1]:nums[0];
      if(m== nums.length-1)
          return nums[m];
-     
-     if(nums[m]<nums[m-1] && nums[m]<=nums[m+1] )
+     if(nums[m]<nums[m-1] && nums[m]<= nums[m+1] )
          return nums[m];
      
+     if(l==h)
+         return nums[l];
      
-     
+    //Recursion calls  
      if(nums[m] > nums[h])
-     {
-         int p =0;
-         for(p=m;p<h;p++)
-         {
-             if(nums[p] == nums[p+1])
-                 continue;
-             else
-                 break;
-         }
          return findMin(nums,m+1,h);
-     }
-         
-     else
-     {
-         int p=0;
-         for(p=m;p>0;--p)
-         {
-             if(nums[p] == nums[p-1])
-                 continue;
-             else
-                 break;
-         }
-         
+     else if((nums[m] < nums[h]))
          return findMin(nums,l,m-1);
-         
-     }
-         
+     else
+         return findMin(nums,l,h-1);
  } 
 
 }
