077f371b41d43c7fcdb9c622ca345dcaaae7a742
Jaharsh Samayan <jaharsh93@gmail.com>
2018-02-17 21:40:15 -0700

    https://leetcode.com/problems/hamming-distance/description/

---
+++ hamming_distance_numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+     
+        int z = x^y;
+        int r=0;
+        while(z>0)
+        {
+            if((z&1)==1)
+                ++r;
+            z= z>>1;
+        }
+        
+        return r;
+    }
+}
